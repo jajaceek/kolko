fecc3e8ec7c1a7d2a80157168cdc3d7010f05ea2
Jacek <jacek@g.pl>
2020-10-08 09:00:08 +0200

    Kolko i rzyzyk - testy

---
+++ src/Test/TicTacToeTest.java
@@ -0,0 +1,50 @@
+package Test;
+
+import static org.junit.Assert.*;
+
+public class TicTacToeTest {
+
+    @org.junit.Test
+    public void firstTurn() {
+    }
+
+    @org.junit.Test
+    public void easyMode() {
+    }
+
+    @org.junit.Test
+    public void mediumMode() {
+    }
+
+    @org.junit.Test
+    public void hardMode() {
+    }
+
+    @org.junit.Test
+    public void reset() {
+    }
+
+    @org.junit.Test
+    public void check() {
+    }
+
+    @org.junit.Test
+    public void xWins() {
+    }
+
+    @org.junit.Test
+    public void oWins() {
+    }
+
+    @org.junit.Test
+    public void draw() {
+    }
+
+    @org.junit.Test
+    public void checkPossibleWin() {
+    }
+
+    @org.junit.Test
+    public void revangeMove() {
+    }
+}
